86e36f6ab0b4015fc7a3d7a505613889339d56db
lizzette_e <lizz.enorme@gmail.com>
2024-04-09 17:47:18 -0500

    Added click event

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+////// variable //////
+
 var button = document.querySelector(".show-cat");
 // console.log(button);
 var cat = document.querySelector(".cat");
 // console.log(cat);
 
+////// user click buttons, function is triggered to show cat image //////
 
+button.addEventListener("click", function() {
+    // press the button and see message in console of its working
+    // console.log("It's working!")
+    cat.classList.add("show");
+});
